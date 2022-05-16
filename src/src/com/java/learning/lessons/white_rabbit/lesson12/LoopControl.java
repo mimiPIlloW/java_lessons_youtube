7644bac795816e8cc7bae6b16531caec61e7e88d
mimiPIllow <erenstonxemin@gmail.com>
2022-05-16 11:53:22 +0300

    white rabbit (lesson 12)

--- src/src/com/java/learning/lessons/white_rabbit/lesson12/LoopControl.java
+++ src/src/com/java/learning/lessons/white_rabbit/lesson12/LoopControl.java
@@ -1,26 +1,32 @@
-package com.java.learning.lessons.white.rabbit.lesson12;
+package com.java.learning.lessons.white_rabbit.lesson12;
 
 public class LoopControl {
 
-    boolean value = true;
-    int a = 5, b = 7;
+   static private boolean value = true;
+    static private int a = 5, b = 7;
 
-    public void foo() {
+    static public void foo() {
 
         while (a < b) {
             System.out.println(a);
             a++;
         }
         System.out.println();
+
         while (true) {
             System.out.println(a);
             a++;
             break;
         }
         System.out.println();
+
         do {
             a++;
             System.out.println(a);
         } while (a < b);
+    }
+
+    public static void main(String[] args) {
+        foo();
     }
 }
