4002b2bf79794890b8e25a8ae874ab0d6ad41b7b
mimiPIllow <erenstonxemin@gmail.com>
2022-05-16 11:51:21 +0300

    white rabbit (lesson 7)

--- src/src/com/java/learning/lessons/white_rabbit/lesson7/Car.java
+++ src/src/com/java/learning/lessons/white_rabbit/lesson7/Car.java
@@ -1,10 +1,26 @@
-package com.java.learning.lessons.white.rabbit.lesson7;
+package com.java.learning.lessons.white_rabbit.lesson7;
 
 public class Car {
-    public String model;  //
-    public String color;
+    private String color;
+    private String model;
 
     public void showColor() {
         System.out.println(color + " " + model);
+    }
+
+    public static void main(String[] args) {
+        Car ourBus = new Car();
+        Car firstBus = new Car();
+        Car secondBus = new Car();
+        ourBus.color = "Green";
+        ourBus.model = "Baw";
+        firstBus.color = "Red";
+        firstBus.model = "SMA";
+        secondBus.color = "Grey";
+        secondBus.model = "Rely";
+
+        ourBus.showColor();
+        firstBus.showColor();
+        secondBus.showColor();
     }
 }
