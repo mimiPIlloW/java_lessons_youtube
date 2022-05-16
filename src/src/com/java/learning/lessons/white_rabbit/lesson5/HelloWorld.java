cd39781559f4a34bf63bc974bd0bb8ecdc2a6cc9
mimiPIllow <erenstonxemin@gmail.com>
2022-05-16 11:50:12 +0300

    white rabbit (lesson 5)

--- src/src/com/java/learning/lessons/white_rabbit/lesson5/HelloWorld.java
+++ src/src/com/java/learning/lessons/white_rabbit/lesson5/HelloWorld.java
@@ -1,6 +1,10 @@
-package com.java.learning.lessons.white.rabbit.lesson5;
+package com.java.learning.lessons.white_rabbit.lesson5;
 
 public class HelloWorld {
+    public static void main(String[] args) {
+        HelloWorld hello = new HelloWorld();
+        hello.hello1();
+    }
 
     public void hello1() {
         System.out.println("Hello World");
