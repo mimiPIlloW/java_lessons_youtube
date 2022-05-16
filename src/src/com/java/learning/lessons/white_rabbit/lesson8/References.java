4a5d0f11161d0757dc126d354d54bd733e16ac79
mimiPIllow <erenstonxemin@gmail.com>
2022-05-16 11:51:50 +0300

    white rabbit (lesson 8)

--- src/src/com/java/learning/lessons/white_rabbit/lesson8/References.java
+++ src/src/com/java/learning/lessons/white_rabbit/lesson8/References.java
@@ -1,10 +1,8 @@
-package com.java.learning.lessons.white.rabbit.lesson8;
-
-import java.util.Locale;
+package com.java.learning.lessons.white_rabbit.lesson8;
 
 public class References {
-    String str = "Моя строка";
-    String str2 = new String("Моя строка2"); //ссылки на объект
+    static public String str = "Моя строка";
+    static public String str2 = new String("Моя строка2"); //ссылки на объект
 
     // ссылочные типы данных, классы,
     Byte a;
@@ -16,8 +14,14 @@ public class References {
     Double g;
     Boolean h;
 
-    public void foo() {
+    static public void foo() {
         new String("Моя строка3");
+        System.out.println(str);
+        System.out.println(str2);
+        System.out.println(new String("Моя строка3"));
     }
 
+    public static void main(String[] args) {
+        foo();
+    }
 }
