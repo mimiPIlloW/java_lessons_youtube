bc447109e6b59aa658409eee902454fc67eed974
mimiPillow <erenstonxemin@gmail.com>
2022-08-22 14:09:36 +0300

    lessons java 12

---
+++ src/src/com/java/learning/lessons/java/lesson12/Human.java
@@ -0,0 +1,13 @@
+package com.java.learning.lessons.java.lesson12;
+
+public class Human {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/src/com/java/learning/lessons/java/lesson12/Main.java
@@ -0,0 +1,9 @@
+package com.java.learning.lessons.java.lesson12;
+
+public class Main {
+    public static void main(String[] args) {
+        Human person = new Human();
+        person.setName("Max");
+        System.out.println(person.getName());
+    }
+}
