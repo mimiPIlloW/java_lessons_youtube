3487e8fbffcd68171b24b64c4aee1664093d3b69
mimiPillow <erenstonxemin@gmail.com>
2022-09-16 18:29:29 +0300

    lessons java 14

---
+++ src/src/com/java/learning/lessons/java/lesson14/Main.java
@@ -0,0 +1,8 @@
+package com.java.learning.lessons.java.lesson14;
+
+public class Main {
+    public static void main(String[] args) {
+        ExampleClass exampleClass = new ExampleClass();
+        exampleClass.method();
+    }
+}
