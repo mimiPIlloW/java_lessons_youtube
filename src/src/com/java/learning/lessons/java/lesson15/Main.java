3c96f3214f0426d52ebb13fd1560b6fbf1168978
mimiPillow <erenstonxemin@gmail.com>
2022-09-18 14:06:11 +0300

    lessons java 15

---
+++ src/src/com/java/learning/lessons/java/lesson15/Main.java
@@ -0,0 +1,13 @@
+package com.java.learning.lessons.java.lesson15;
+
+public class Main implements ISomeInterface {
+    @Override
+    public void sumTwoNumber(int one, int two) {
+
+    }
+
+    @Override
+    public void someMethod() {
+
+    }
+}
