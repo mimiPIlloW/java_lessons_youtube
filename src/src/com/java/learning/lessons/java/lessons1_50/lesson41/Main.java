2912613387b5e354c0e3e0ec33a7a5dec685c655
mimiPillow <erenstonxemin@gmail.com>
2022-09-29 14:48:02 +0300

    lessons java 41

---
+++ src/src/com/java/learning/lessons/java/lessons1_50/lesson41/Main.java
@@ -0,0 +1,10 @@
+package com.java.learning.lessons.java.lessons1_50.lesson41;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3};
+        for (int a : arr){
+            System.out.println(a);
+        }
+    }
+}
