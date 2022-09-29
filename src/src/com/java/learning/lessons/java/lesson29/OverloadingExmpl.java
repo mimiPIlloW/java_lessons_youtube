72091e909e45888464aad8135b5803d43454f790
mimiPillow <erenstonxemin@gmail.com>
2022-09-29 11:20:02 +0300

    lessons java 29

---
+++ src/src/com/java/learning/lessons/java/lesson29/OverloadingExmpl.java
@@ -0,0 +1,46 @@
+package com.java.learning.lessons.java.lesson29;
+
+public class OverloadingExmpl {
+    public static void main(String[] args) {
+        OverloadingExmpl overloadingExmpl = new OverloadingExmpl();
+        short b = 5;
+        overloadingExmpl.method(b);
+        overloadingExmpl.method((long) b);
+    }
+
+    void method(int i) {
+        System.out.println("int");
+    }
+
+    void method(long i) {
+        System.out.println("long");
+    }
+
+    void method(Integer i) {
+        System.out.println("Integer");
+    }
+
+    void method(double i) {
+        System.out.println("double");
+    }
+
+    void method(byte... i) {
+        System.out.println("byte");
+    }
+
+    void method(byte i) {
+        System.out.println("byte");
+    }
+
+    void method(Byte i) {
+        System.out.println("long");
+    }
+
+    void method(Object i) {
+        System.out.println("long");
+    }
+
+    void method(Object... i) {
+        System.out.println("long");
+    }
+}
